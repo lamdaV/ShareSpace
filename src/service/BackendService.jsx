234960a00118004e5617f9db0e14785a37de5076
David Lam <dav_lam@ymail.com>
2018-11-24 15:55:30 -0800

    removing api endpoint

--- src/service/BackendService.jsx
+++ src/service/BackendService.jsx
@@ -6,38 +6,38 @@ class BackendService {
   }
 
   verify() {
-    return this.service.get("/verify");
+    return this.service.get("/api/verify");
   }
 
   register(username, password, invite) {
-    return this.service.put("/register", {username, password, invite});
+    return this.service.put("/api/register", {username, password, invite});
   }
 
   login(username, password) {
-    return this.service.post("/login", {username, password});
+    return this.service.post("/api/login", {username, password});
   }
 
   logout() {
-    return this.service.get("/logout");
+    return this.service.get("/api/logout");
   }
 
   listFiles(path = null) {
     if (path) {
-      return this.service.get("/user/list", {params: {path}});
+      return this.service.get("/api/user/list", {params: {path}});
     }
-    return this.service.get("/user/list");
+    return this.service.get("/api/user/list");
   }
 
   download(path) {
-    return this.service.get("/user/download", {params: {path}, responseType: "blob"});
+    return this.service.get("/api/user/download", {params: {path}, responseType: "blob"});
   }
 
   delete(path) {
-    return this.service.delete("/user/delete", {params: {path}});
+    return this.service.delete("/api/user/delete", {params: {path}});
   }
 
   rename(oldPath, newPath) {
-    return this.service.post("/user/rename", {oldPath, newPath});
+    return this.service.post("/api/user/rename", {oldPath, newPath});
   }
 
   upload(file, path) {
@@ -46,14 +46,14 @@ class BackendService {
     if (path) {
       formData.append("path", path);
     }
-    return this.service.put("/user/upload", formData);
+    return this.service.put("/api/user/upload", formData);
   }
 
   createDirectory(path, directory) {
     if (path) {
-      return this.service.put("/user/directory", {directory});
+      return this.service.put("/api/user/directory", {directory});
     } 
-    return this.service.put("/user/directory", {path, directory});
+    return this.service.put("/api/user/directory", {path, directory});
   }
 }
 
